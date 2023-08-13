b3faa455ab0432da6865bc6785219f1dcfa47114
Lubomyr Lypetskyi <liubomyr.lypetskyi@gmail.com>
2023-08-13 21:12:01 +0300

    added HousingLocationComponent

---
+++ src/app/housing-location/housing-location.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-housing-location',
+  templateUrl: './housing-location.component.html',
+  styleUrls: ['./housing-location.component.scss']
+})
+export class HousingLocationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
