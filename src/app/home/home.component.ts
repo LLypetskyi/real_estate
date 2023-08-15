63b632f0fe08a86f4af6b94cbe0d7f81c9ab3af4
Lubomyr Lypetskyi <liubomyr.lypetskyi@gmail.com>
2023-08-15 10:11:46 +0300

    Added the search feature to app

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,8 +10,20 @@ import { HousingService } from '../services/housing.service';
 })
 export class HomeComponent {
   housingLocationList: HousingLocation[] = [];
+  filteredLocationList: HousingLocation[] = [];
 
   constructor(private housingService: HousingService) {
     this.housingLocationList = this.housingService.getAllHousingLocations();
+    this.filteredLocationList = this.housingLocationList;
+  }
+
+  filterResults(text: string) {
+    if (!text) {
+      this.filteredLocationList = this.housingLocationList;
+    }
+
+    this.filteredLocationList = this.housingLocationList.filter(
+      housingLocation => housingLocation?.city.toLowerCase().includes(text.toLowerCase())
+    );
   }
 }
